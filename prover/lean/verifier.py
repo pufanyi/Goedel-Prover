829767d2d491f9c8951f5b06137a81f5aa527876
Pu Fanyi <pufanyi@gmail.com>
2025-07-14 23:14:41 +0800

    Update verifier.py

--- prover/lean/verifier.py
+++ prover/lean/verifier.py
@@ -5,7 +5,6 @@ import time
 import json
 import ctypes
 import resource
-import tempfile
 import traceback
 import threading
 import subprocess
@@ -25,6 +24,7 @@ DEFAULT_LEAN_WORKSPACE = 'mathlib4/'
 
 
 def verify_lean4_file(code, lake_path=DEFAULT_LAKE_PATH, lean_workspace=DEFAULT_LEAN_WORKSPACE, last_env=None, verbose=False, timeout=300, allTactics=False, ast=False, premises=False, tactics=False):
+    # code = json.loads(code)["cmd"].strip()
     command = dict(cmd=code, allTactics=allTactics, ast=ast, tactics=tactics, premises=premises)
     if last_env is not None:
         command.update(env=last_env)
@@ -34,10 +34,8 @@ def verify_lean4_file(code, lake_path=DEFAULT_LAKE_PATH, lean_workspace=DEFAULT_
     start_time = time.time()
     system_messages = ''
     try:
-        with tempfile.TemporaryFile(mode='w+', encoding='utf-8') as temp_file:
-            temp_file.write(message_str + "\r\n\r\n")
-            temp_file.seek(0)
-            outputs = subprocess.run([lake_path, "exe", 'repl'], stdin=temp_file, capture_output=True, text=True, cwd=lean_workspace, timeout=timeout)
+        outputs = subprocess.run([lake_path, "exe", 'repl'], input=message_str, capture_output=True, text=True, cwd=lean_workspace)
+        print(outputs.stdout)
         result = json.loads(outputs.stdout)
         ast_results = lean4_parser(code, result['ast']) if 'ast' in result and result['ast'] else {}
         result = {
